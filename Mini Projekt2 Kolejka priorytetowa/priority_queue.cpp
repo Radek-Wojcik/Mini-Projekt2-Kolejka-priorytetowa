17a5cf3e4f43f4fa7a1a54ab9a802439ce5e0df2
Radek Wójcik <radekw04@gmail.com>
2025-05-08 19:33:40 +0200

    Implementacja konstruktora

--- Mini Projekt2 Kolejka priorytetowa/priority_queue.cpp
+++ Mini Projekt2 Kolejka priorytetowa/priority_queue.cpp
@@ -1,4 +1,18 @@
 #include "priority_queue.h"
 
 #include <iostream>
+#include <string>
+#include <limits>
+
+template <typename T>
+PriorityQueue<T>::PriorityQueue(int minPrio, int maxPrio)
+    : currentOrder(0),
+    rng(static_cast<unsigned int>(std::time(nullptr))),
+    minPriority(minPrio),
+    maxPriority(maxPrio) {
+    // Sprawdzenie, ¿e maxPriority jest wiêksze ni¿ minPriority
+    if (minPriority >= maxPriority) {
+        maxPriority = minPriority + 1000;     
+    }
+}
 
