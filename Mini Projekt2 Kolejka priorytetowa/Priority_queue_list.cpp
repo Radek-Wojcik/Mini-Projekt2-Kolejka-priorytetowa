b5311ed36bed92b9b8bed089c2bda6a1506b24d9
Oliwia Pająk <oliwiapajak@gmail.com>
2025-05-08 23:18:10 +0200

    Implementacja konstruktora, destruktora i gettera

--- Mini Projekt2 Kolejka priorytetowa/Priority_queue_list.cpp
+++ Mini Projekt2 Kolejka priorytetowa/Priority_queue_list.cpp
@@ -1,3 +1,23 @@
 #include "Priority_queue_list.h"
 #include<iostream>
 #include<string>
+
+template<typename T>
+ListQueue<T>::ListQueue() : head(nullptr), tail(nullptr), size(0) {}
+
+template<typename T>
+int ListQueue<T>::get_size() const {
+	return size;
+}
+
+
+
+template<typename T>
+ListQueue<T>::~LinkedList() {
+	while (head != nullptr) {
+		LNode* temp = head;
+		head = head->next;
+		delete temp;
+	}
+	tail = nullptr;
+}
