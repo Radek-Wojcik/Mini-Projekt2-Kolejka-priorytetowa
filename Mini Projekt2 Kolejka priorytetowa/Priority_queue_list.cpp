d1d898f126e8e0e6a9271018677a460c690c61dc
Oliwia Pająk <oliwiapajak@gmail.com>
2025-05-12 19:50:01 +0200

    Implementacja funkcji insert

--- Mini Projekt2 Kolejka priorytetowa/Priority_queue_list.cpp
+++ Mini Projekt2 Kolejka priorytetowa/Priority_queue_list.cpp
@@ -10,10 +10,31 @@ int ListQueue<T>::get_size() const {
 	return size;
 }
 
-
+template<typename T>
+void ListQueue<T>::insert(T value, int priority) {
+	LNode* newNode = new LNode(value, priority, head);
+	//Jesli lista jest pusta lub nowy ma wiekszy priorytet niz pierwszy element
+	if (head == nullptr || priority > head->priority) {
+		newNode->next = head;
+		head = newNode;
+		if (tail == nultptr) tail = newNode;
+	}
+	else {
+		//szukamy miejsca do wstawienia
+		LNode* current = head;
+		while (current->next != nullptr && current->next->priority >= priority) {
+			current = current->next;
+		}
+		newNode->next = current->next;
+		current->next = newNode;
+		//jesli wstawiamy na koniec aktualizujemy tail
+		if (newNode->next == nullptr) tail = newNode;
+	}
+	size++;
+}
 
 template<typename T>
-ListQueue<T>::~LinkedList() {
+ListQueue<T>::~ListQueue() {
 	while (head != nullptr) {
 		LNode* temp = head;
 		head = head->next;
